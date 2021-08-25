b2c2edb4ef29983ec8d2a2d374a05fd2af26118c
mghddev <mghddev@gmail.com>
2021-08-25 17:30:25 +0430

    Use Request class of laravel for validation

--- app/Http/Controllers/API/HttpApiCalculatePrice.php
+++ app/Http/Controllers/API/HttpApiCalculatePrice.php
@@ -3,38 +3,30 @@ namespace App\Http\Controllers\API;
 
 
 use App\Action\Goods\ActionCalculateTotalPrice;
+use App\Http\Requests\GoodsItemsRequest;
 use App\Http\Resources\PriceResource;
-use App\Http\Validation\ShoppingValidation;
 use Exception;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Validation\ValidationException;
 
 class HttpApiCalculatePrice
 {
-    private ShoppingValidation $validation;
+
     private ActionCalculateTotalPrice $actionCalculateTotalPrice;
 
-    /**
-     * HttpApiCalculatePrice constructor.
-     * @param ShoppingValidation $validation
-     * @param ActionCalculateTotalPrice $actionCalculateTotalPrice
-     */
-    public function __construct(ShoppingValidation $validation, ActionCalculateTotalPrice $actionCalculateTotalPrice)
+    public function __construct(ActionCalculateTotalPrice $actionCalculateTotalPrice)
     {
-        $this->validation = $validation;
         $this->actionCalculateTotalPrice = $actionCalculateTotalPrice;
     }
 
     /**
-     * @return JsonResponse
      * @throws ValidationException
      * @throws Exception
      */
-    public function __invoke(): JsonResponse
+    public function __invoke(GoodsItemsRequest $request): JsonResponse
     {
-        $data = request()->all();
-        $this->validation->calculateTotalPrice($data);
-        $price = $this->actionCalculateTotalPrice->__invoke($data['items']);
+        $price = $this->actionCalculateTotalPrice->__invoke($request->get('items'));
+
         return (new PriceResource($price))->response();
     }
 }

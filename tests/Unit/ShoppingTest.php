c94621cfd79ed6b84085bd81eb2065f1bf6df5ed
mghddev <mghddev@gmail.com>
2021-08-25 19:33:33 +0430

    Fix variable name in test

--- tests/Unit/ShoppingTest.php
+++ tests/Unit/ShoppingTest.php
@@ -7,30 +7,33 @@ use PHPUnit\Framework\TestCase;
 
 class ShoppingTest extends TestCase
 {
-    /**
-     * A basic unit test example.
-     *
-     * @return void
-     */
-    public function test_example()
-    {
 
-        $calculator = new ActionCoinChangeCalculation();
+    /**
+     * A unit test for coin change calculation function.
+     */
+    public function testActionCoinChangeCalculation_findCombinationsUtil()
+    {
+        //Arrange
+        $sut = new ActionCoinChangeCalculation();
         $numbers = [2, 5, 8];
         $quantity = 20;
-
-        //Different combinations from 2,5,8 that their summation is 20 are as below:
-        // [[2,2,2,2,2,2,2,2,2,2], [2,2,2,2,2,2,8], [2,2,2,2,2,5,5], [2,2,8,8], [2,5,5,8], [5,5,5,5]]
-        $arr = $calculator->__invoke($quantity, $numbers);
-        $this->assertIsArray($arr);
-        $this->assertCount(6, $arr);
-        $this->assertEquals(array(
+        $expected = array(
             [2,2,2,2,2,2,2,2,2,2],
             [2,2,2,2,2,2,8],
             [2,2,2,2,2,5,5],
             [2,2,8,8],
             [2,5,5,8],
-            [5,5,5,5]),
-            $arr);
+            [5,5,5,5]);
+
+        //Assign
+        //Different combinations from 2,5,8 that their summation is 20 are as below:
+        // [[2,2,2,2,2,2,2,2,2,2], [2,2,2,2,2,2,8], [2,2,2,2,2,5,5], [2,2,8,8], [2,5,5,8], [5,5,5,5]]
+        $actual = $sut->__invoke($quantity, $numbers);
+
+        //Assert
+        $this->assertIsArray($actual);
+        $this->assertCount(6, $actual);
+        $this->assertEquals($expected, $actual);
     }
+
 }
